739d20110b54bfe78a21e414e081b690a310ff91
Paweł Hanusek <hanss007@gmail.com>
2021-02-23 20:31:44 +0100

    remove background from itemId

--- pages/items/[itemId].js
+++ pages/items/[itemId].js
@@ -15,7 +15,7 @@ const BackgroundWrapper = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    background-image: url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1867&q=80");
+    /* background-image: url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1867&q=80"); */
     background-size: cover;
 
     width: 100%;
