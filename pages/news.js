0c5b7e767d62289c2811e1c9a084d84b5142d798
Paweł Hanusek <hanss007@gmail.com>
2021-02-24 20:09:40 +0100

    background removed from news

--- pages/news.js
+++ pages/news.js
@@ -15,8 +15,8 @@ const WrapperStyled = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    background-image: url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1867&q=80");
-    background-size: cover;
+    /* background-image: url("https://images.unsplash.com/photo-1531685250784-7569952593d2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1867&q=80");
+    background-size: cover; */
 
     width: 100%;
     height: 100%;
