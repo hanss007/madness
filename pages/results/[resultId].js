d0a1ecc195ebc9e449ac8d189bcc6e6a67fe9c7e
Paweł Hanusek <hanss007@gmail.com>
2021-02-27 11:42:17 +0100

    add fetchQuery

--- pages/results/[resultId].js
+++ pages/results/[resultId].js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { baseUrl, fetchQuery } from "../../utils";
 import GlobalStyle from "../../theme/GlobalStyle";
 import Navbar from "../../components/Navbar";
 import BurgerMenu from "../../components/BurgerMenu";
