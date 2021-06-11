667bd57c7f3a75b3730bcc802e5574b9afd21722
Paweł Hanusek <hanss007@gmail.com>
2021-06-11 12:53:45 +0200

    edit calenadar

--- data/events.js
+++ data/events.js
@@ -4,28 +4,29 @@ export const events = [
     month: "02.2021",
     content: "XV Futsalowe Mistrzostwa Polski Górników i Przyjaciół-ŻORY 2021",
   },
-
-  {
-    day: "09",
-    month: "05.2021",
-    content: `"VIII KLIMA CUP-GRAMY DLA EWELINY-charytatywny Turniej piłki siatkowej i futsalu-CHOCIANÓW 2021"`,
-  },
   {
     day: "15-16",
     month: "05.2021",
     content:
       "XI Amatorskie Mistrzostwa Polski Górników i Przyjaciół w piłce siatkowej-CHOCIANÓW 2021",
   },
+
   {
     day: "30",
     month: "05.2021",
-    content:
-      "X Oldbojowe Mistrzostwa Polski Górników i Przyjaciół-CHOCIANÓW 2021",
+    content: `"VIII KLIMA CUP-GRAMY DLA EWELINY-charytatywny Turniej piłki siatkowej i futsalu-CHOCIANÓW 2021"`,
   },
+
   {
-    day: "25-27",
+    day: "26-27",
     month: "06.2021",
     content:
       "Amatorskie Mistrzostwa Polski Górników i Przyjaciół w piłce nożnej 6-osobowej w Gniewinie",
+  },
+  {
+    day: "Do",
+    month: "ustalenia",
+    content:
+      "X Oldbojowe Mistrzostwa Polski Górników i Przyjaciół-CHOCIANÓW 2021",
   },
 ];
